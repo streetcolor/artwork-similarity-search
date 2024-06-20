a2d017dd0837987553cfbf3d2bcbf634b33e3ca6
streetcolor <sebastien.tetard@yahoo.fr>
2024-06-20 21:08:11 +0000

    Fix

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import streamlit as st
 import io
 from qdrant.vector_searcher import VectorSearch
-from config import QDRANT_URL, QDRANT_KEY, EMBEDDER, COLLECTION_NAME
+from config import QDRANT_URL, QDRANT_KEY, EMBEDDER, COLLECTION_NAME, IMAGES_DIR
 from PIL import Image
-from data.from_gcp_bucket import GCP
 import requests
-from io import BytesIO
-
 st.set_page_config(
     page_title="Artwork Search", layout="centered", page_icon="./images/icon.png"
 )
@@ -18,7 +15,6 @@ def load_search_object():
                             qdrant_key=QDRANT_KEY, collection_name=COLLECTION_NAME)
 
 vectorsearch = load_search_object()
-gcp = GCP()
 
 st.sidebar.image("images/header_sidebar.png")
 st.sidebar.title("Vector Search Engine")
@@ -53,7 +49,6 @@ else:
     artwork_desc = st.text_input("Describe the artwork")
 
 if image_bytes or artwork_desc:
-    artists_data = gcp.get_artists_data()
 
     k = st.slider(label='Choose how many similar images to get',
                   min_value=1, max_value=10, step=1, value=3)
@@ -71,24 +66,17 @@ if image_bytes or artwork_desc:
             with st.spinner("Searching for atwork that matches your description..."):
                 search_result = vectorsearch.search(artwork_desc, k)
 
-        artists_data = gcp.get_artists_data()
-
         
         st.title("Image search result")
         for id, r in enumerate(search_result):
-            st.subheader(f"{r['artist']}")
+            st.subheader(f"{r['artist']} |{id} ")
+            st.write(id)
             st.markdown(
-                f"{artists_data[r['artist']]['nationality']} - (*{artists_data[r['artist']]['years']}*)")
-            st.markdown(f"Genre: {artists_data[r['artist']]['genre']}")
-            st.write(artists_data[r['artist']]['bio'])
-            st.markdown(
-                f"[*Learn more*]({artists_data[r['artist']]['wikipedia']})")
-            st.image(gcp.get_image_url(
-                r['image_name']), caption=r['artist'], width=400)
-            with st.expander(f"See artworks from {r['artist']}"):
-                c1, c2, c3, c4 = st.columns(4)
-                for img_url, c in zip(gcp.get_artist_artwork(r['artist'] ,4), [c1,c2,c3,c4]):
-                    r = requests.get(img_url)
-                    image = Image.open(BytesIO(r.content)).resize((400,400), Image.LANCZOS)
-                    c.image(image)
+                f"[*Learn more*]({id})")
+
+            url = "https://media.artsper.com/artwork/{id}_1_m.jpg".format(id=r['image_name'].split('_')[-1].split('.')[0])
+
+            image = Image.open(requests.get(url, stream=True).raw).resize((400, 400), Image.LANCZOS)
+            st.image(image)
+
             st.divider()
--- config.py
+++ config.py
@@ -1,19 +1,12 @@
 import os
 import streamlit as st
 
-
-
 CODE_DIR = os.path.dirname(__file__)
 ROOT_DIR = os.path.dirname(CODE_DIR)
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
-IMAGES_DIR = os.path.join(CODE_DIR ,'data', 'artwork_data','images','combined')
-KAGGLE_USERNAME = st.secrets['Kaggle']['KAGGLE_USERNAME']
-KAGGLE_KEY = st.secrets['Kaggle']['KAGGLE_KEY']
+IMAGES_DIR = os.path.join(CODE_DIR ,'data', 'artsper')
 
 COLLECTION_NAME = st.secrets['Database']['COLLECTION_NAME']
-
 EMBEDDER = st.secrets['Database']['EMBEDDER_NAME']
 QDRANT_URL = st.secrets['Database']['QDRANT_URL']
 QDRANT_KEY = st.secrets['Database']['QDRANT_KEY']
-
-GCP_CREDS = st.secrets["gcp_service_account"]
